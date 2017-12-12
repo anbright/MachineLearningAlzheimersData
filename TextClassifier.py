6d9f827be7e8472a58443f7e43a8318eb45564c9
anbright <anbright@sas.upenn.edu>
2017-12-11 21:32:25 -0500

    added sentiment analysis

--- TextClassifier.py
+++ TextClassifier.py
@@ -7,15 +7,17 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import accuracy_score, recall_score, precision_score, roc_curve, auc
 from sklearn import model_selection
 
-from sklearn.naive_bayes import BernoulliNB
+from sklearn.naive_bayes import BernoulliNB, MultinomialNB
 
 from sklearn.svm import SVC
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.decomposition import TruncatedSVD
 from sklearn.preprocessing import label_binarize
+from sklearn.metrics import roc_curve, auc
 
 from sklearn.externals import joblib
+from scipy import interp
 
 from pymongo import MongoClient
 
@@ -34,9 +36,11 @@ stemmer = SnowballStemmer("english", ignore_stopwords=True)
 # 	stems = [stemmer.stem(item) for item in text]
 # 	return stems
 
+n_rows = 29096
+
 y = []
 x = []
-for post in agingcare.find({"resource_topic": {"$nin": excluded_categories}}, {"resource_topic":1, "text": 1, "question": 1, "question_body":1}):
+for post in agingcare.find({"resource_topic": {"$nin": excluded_categories}}, {"resource_topic":1, "text": 1, "question": 1, "question_body":1}).limit(35000):
 	if post['text'] and post['question'] and post['question_body']:
 		tmp = []
 		tmp.append(post['text'])
@@ -45,6 +49,9 @@ for post in agingcare.find({"resource_topic": {"$nin": excluded_categories}}, {"
 
 		y.append(post['resource_topic'])
 		x.append(''.join(tmp))
+
+# x = np.asarray(x)
+# y = np.asarray(y)
 
 x_train, x_test, y_train, y_test = model_selection.train_test_split(x, y, test_size=0.2)
 
@@ -62,8 +69,8 @@ x_test = tfidf_vect.transform(x_test)
 # x_test = svd.transform(x_test)
 
 ## Naive Bayes classifier
-clf_NB = BernoulliNB()
-clf_NB.fit(x_train, y_train)
+clf_NB = MultinomialNB(alpha=0.2)
+trained_NB = clf_NB.fit(x_train, y_train)
 
 y_hat = clf_NB.predict(x_test)
 
@@ -81,6 +88,70 @@ print(accuracy_score(y_hat, y_test))
 print(recall_score(y_hat, y_test, average='weighted'))
 print(precision_score(y_hat, y_test, average='weighted'))
 
-joblib.dump(tfidf_vect, 'vectorizer.pkl')
-joblib.dump(clf_SVC, 'SVC.pkl')
-joblib.dump(clf_NB, 'NB.pkl')
+# joblib.dump(tfidf_vect, 'vectorizer.pkl')
+# joblib.dump(clf_SVC, 'SVC.pkl')
+# joblib.dump(clf_NB, 'NB.pkl')
+
+## ROC Curves
+lw = 2
+plt.figure()
+
+categories = np.unique(y)
+n_classes = categories.shape[0]
+y_test_binary = label_binarize(y_test, classes=categories)
+
+fpr = dict()
+tpr = dict()
+roc_auc = dict()
+
+# NB
+decision_NB = clf_NB.predict_proba(x_test)
+
+for i in range(n_classes):
+	fpr[i], tpr[i], _ = roc_curve(y_test_binary[:, i], decision_NB[:, i])
+
+all_fpr = np.unique(np.concatenate([fpr[i] for i in range(n_classes)]))
+
+mean_tpr = np.zeros_like(all_fpr)
+for i in range(n_classes):
+    mean_tpr += interp(all_fpr, fpr[i], tpr[i])
+mean_tpr /= n_classes
+
+fpr["macro"] = all_fpr
+tpr["macro"] = mean_tpr
+roc_auc["macro"] = auc(fpr["macro"], tpr["macro"])
+
+
+plt.plot(fpr["macro"], tpr["macro"], color='darkorange', linewidth=4, label='Naive Bayes')
+
+## ROC for SVC
+
+decision_SVC = clf_SVC.decision_function(x_test)
+
+for i in range(n_classes):
+	fpr[i], tpr[i], _ = roc_curve(y_test_binary[:, i], decision_SVC[:, i])
+
+all_fpr = np.unique(np.concatenate([fpr[i] for i in range(n_classes)]))
+
+mean_tpr = np.zeros_like(all_fpr)
+for i in range(n_classes):
+    mean_tpr += interp(all_fpr, fpr[i], tpr[i])
+mean_tpr /= n_classes
+
+fpr["macro"] = all_fpr
+tpr["macro"] = mean_tpr
+roc_auc["macro"] = auc(fpr["macro"], tpr["macro"])
+
+plt.plot(fpr["macro"], tpr["macro"], color='navy', linewidth=4, label='SVC')
+
+
+# Finish plotting
+
+plt.plot([0, 1], [0, 1], 'k--', lw=lw)
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('ROC Curves')
+plt.legend(loc="lower right")
+plt.show()
