986485d00c4e1580d515aa8bb84bc0250927c110
mciul <mciul@ldc.upenn.edu>
2017-12-12 08:24:54 -0500

    Add grid search to NBClassifier

--- NBClassifier.py
+++ NBClassifier.py
@@ -8,12 +8,7 @@ from sklearn.metrics import accuracy_score, recall_score, precision_score, confu
 from sklearn import model_selection
 
 from sklearn.naive_bayes import BernoulliNB
-
-from sklearn.svm import SVC
-from sklearn.metrics.pairwise import cosine_similarity
-from sklearn.multiclass import OneVsRestClassifier
-from sklearn.decomposition import TruncatedSVD
-from sklearn.preprocessing import label_binarize
+from sklearn.pipeline import Pipeline
 
 from sklearn.externals import joblib
 
@@ -40,12 +35,12 @@ for post in agingcare.find({"resource_topic": {"$nin": excluded_categories}}, {"
 
 x_train, x_test, y_train, y_test = model_selection.train_test_split(x, y, test_size=0.2)
 
+labels = np.unique(y_train)
 print("n=", len(x))
 
 ## Tf-Idf vectorization
-tfidf_vect = TfidfVectorizer(lowercase=True, stop_words='english', sublinear_tf=True)
-x_train = tfidf_vect.fit_transform(x_train)
-x_test = tfidf_vect.transform(x_test)
+tfidf_vect = TfidfVectorizer(lowercase=True, stop_words='english',
+        sublinear_tf=True, max_df=1.0)
 
 ## Dimensionality reduction
 # svd = TruncatedSVD(n_components=500, random_state=42)
@@ -54,18 +49,39 @@ x_test = tfidf_vect.transform(x_test)
 
 ## Naive Bayes classifier
 clf_NB = BernoulliNB()
-clf_NB.fit(x_train, y_train)
 
-y_hat = clf_NB.predict(x_test)
+pipeline = Pipeline([
+    ('tfidf', tfidf_vect),
+    ('nb', clf_NB)
+    ])
 
+parameters = {
+    'tfidf__max_df': (0.1, 0.5, 1.0),
+    'tfidf__ngram_range': ((1,1), (1,2)),
+    'nb__alpha': (0.2, 0.6, 1.0),
+    'nb__binarize': (0.0, 0.01, 0.1, 0.5, 0.9)
+    }
+
+grid = model_selection.GridSearchCV(pipeline, parameters)
+
+clf = grid
+clf.fit(x_train, y_train)
+y_hat = clf.predict(x_test)
+
+print ("grid search params: ")
+best_parameters = grid.best_estimator_.get_params()
+for param_name in sorted(parameters.keys()):
+    print("\t%s: %r" % (param_name, best_parameters[param_name]))
 print("NB accuracy: ", accuracy_score(y_test, y_hat))
 print("NB recall: ", recall_score(y_test, y_hat, average='weighted'))
 print("NB precision: ", precision_score(y_test, y_hat, average='weighted'))
 
 print "y_test[0]=", y_test[0]
 print "y_hat[0]=", y_hat[0]
+print "Labels:"
+print labels
 print "NB confusion:"
-print confusion_matrix(y_test, y_hat)
+print confusion_matrix(y_test, y_hat, labels=labels)
 
 joblib.dump(tfidf_vect, 'vectorizer_2.pkl')
 joblib.dump(clf_NB, 'NB.pkl')
